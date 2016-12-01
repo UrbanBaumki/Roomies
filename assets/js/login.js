4586c0fad8a306cffd0cc5554eaf57e1f6104147
NKambic <nejc.kambic@gmail.com>
2016-12-01 11:52:02 +0100

    login and registration

---
+++ assets/js/login.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	/*
+        Login form validation
+    */
+	$('.login-form input[type="text"], .login-form input[type="password"]').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+        var send  =true;
+        var values = [];
+    	$(this).find('input[type="text"], input[type="password"]').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+                send = false;
+    			$(this).addClass('input-error');
+    		}
+    		else {
+                e.preventDefault();
+    			$(this).removeClass('input-error');
+                values.push( $(this).val() )
+    		}
+    	});
+        if(send){
+            e.preventDefault(); 
+            $.ajax({
+                            type: "POST",
+                            url: "login.php",         
+                            data:{username:values[0],password:values[1],registerRoom:values[2]},
+                            success:function(data){
+                                e.preventDefault();
+                                var result = eval(data);
+                                if(result[0] == 401){
+                                    $("#errorMessageLogin").css("color", "red");
+                                    document.getElementById("errorMessageLogin").innerHTML = result[1];
+                                }
+                                else{
+                                    document.getElementById("errorMessageLogin").innerHTML = "";
+                                }
+                            },
+                            error: function (request, status, error) {
+                                e.preventDefault();
+                            }
+
+            });
+        }
+    	
+    });
+});
---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$registerRoom=$_POST['registerRoom'];
+	$url = 'http://lazykiller.herokuapp.com/login';
+		//Initiate cURL.
+	$ch = curl_init($url);
+	//The JSON data.
+	$data = array(
+		"username" => $username, 
+		"password" => $password,
+		"room" => array("name" => $registerRoom)
+		); 
+	
+	//Encode the array into JSON.
+	$data_json = json_encode($data);
+	//Tell cURL that we want to send a POST request.
+	curl_setopt($ch, CURLOPT_POST, 1);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	//Attach our encoded JSON string to the POST fields.
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+	//Set the content type to application/json
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+	//Execute the request
+	$response = json_decode(curl_exec($ch));
+	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+	curl_close($ch);
+	if($http_code ==401){// return error
+		$return = array($http_code,$response->error);
+		echo json_encode($return);
+	}
+	else{
+		$return = array($http_code,$response ->_id, $response ->room,$response->username);
+		echo json_encode($return);
+	}
+?>
